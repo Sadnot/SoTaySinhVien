f7ea70a1dc90424689063e5da874d8f4d45f3f45
Sadnot <92632621+Sadnot@users.noreply.github.com>
2022-03-16 16:43:16 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+let input = document.querySelector('input')
+let button = document.querySelector('.button')
+let form = document.querySelector('form')
+let todos = document.querySelector('.todos')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let val = input.value.trim()
+    if (val) {
+        addTodoElement({
+            text: val,
+        })
+        saveTodoList()
+    } 
+
+    input.value = ''
+})
+
+
+function addTodoElement(todo) {
+    let li = document.createElement('li') 
+    li.innerHTML = `
+                        <span>${todo.text}</span>
+                        <i class="fas fa-trash"></i>
+                    `
+
+    if (todo.status === 'completed') {
+        li.setAttribute('class', 'completed')
+    }
+
+    li.addEventListener('click', function(e) {
+        this.classList.toggle('completed')
+        saveTodoList()
+    })
+
+    li.querySelector('i').addEventListener('click', function(e) {
+        this.parentElement.remove()
+        saveTodoList()
+    })
+
+    todos.appendChild(li)
+}
+
+function saveTodoList() {
+    let todoList = document.querySelectorAll('li')
+    let todoStorage = []
+    
+    todoList.forEach(function(item) {
+        let text = item.querySelector('span').innerText 
+        let status = item.getAttribute('class')
+
+        todoStorage.push({
+            text,
+            status
+        })
+    })
+    localStorage.setItem('todoList', JSON.stringify(todoStorage))
+}
+
+function init() {
+    let data = JSON.parse(localStorage.getItem('todoList'))
+    data.forEach(function (item) {
+        addTodoElement(item)
+    })
+}
+
+init()
